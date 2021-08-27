47f374dc66bbaf08fe2d3929dd4a7b1a75078694
Matt Hill <matt.hill.email@gmail.com>
2021-08-26 20:39:46 -0700

    add adn delete work

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
-import React, { useEffect, useState } from "react"
-
-import Amplify, { API, graphqlOperation } from "aws-amplify"
-
-import "semantic-ui-css/semantic.min.css"
-import { Button, Container, Icon } from "semantic-ui-react"
-
-import awsConfig from "./aws-exports"
-import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react"
-import { listLists } from "./graphql/queries"
-import MainHeader from "./components/headers/MainHeader"
-import ListItems from "./ListItems"
-import Lists from "./components/Lists/Lists"
-
-Amplify.configure(awsConfig)
+import React from "react"
+import Main from "./Main"
 
 function App() {
-  const [lists, setLists] = useState([])
-
-  async function fetchList() {
-    console.log("fetch list") // zzz
-    const data = await API.graphql(graphqlOperation(listLists)).catch(
-      (error) => console.log("error", error) // zzz
-    )
-    console.log("data", data) // zzz
-    const lists = data?.data?.listLists?.items || []
-    console.log("lists", lists) // zzz
-    setLists(lists)
-  }
-
-  useEffect(() => {
-    fetchList()
-  }, [])
-
-  return (
-    <AmplifyAuthenticator>
-      <AmplifySignOut />
-
-      <div className="App">
-        <MainHeader />
-        <Lists lists={lists} />
-      </div>
-    </AmplifyAuthenticator>
-  )
+  return <Main />
 }
 
 export default App
