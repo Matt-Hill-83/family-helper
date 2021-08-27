eeacf61c30652dbb5a274b8bc9fb193074819954
Matt Hill <matt.hill.email@gmail.com>
2021-08-26 19:50:32 -0700

    messing with api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-import Amplify from "aws-amplify"
+import Amplify, { API, graphqlOperation } from "aws-amplify"
+
 import awsConfig from "./aws-exports"
 import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react"
+import { listLists } from "./graphql/queries"
 
 Amplify.configure(awsConfig)
 
 function App() {
+  const [list, setlist] = useState([])
+
+  async function fetchList() {
+    console.log("fetch list") // zzz
+    const test = await API.graphql(graphqlOperation(listLists)).catch(
+      (error) => console.log("error", error) // zzz
+    )
+    console.log("test", test) // zzz
+    // const { data } = await API.graphql(graphqlOperation(listLists))
+    // console.log("data", data) // zzz
+  }
+
+  useEffect(() => {
+    fetchList()
+  }, [])
+
   return (
     <AmplifyAuthenticator>
       <AmplifySignOut />
 
-      <div className="App"></div>
+      <div className="App">test</div>
     </AmplifyAuthenticator>
   )
 }
